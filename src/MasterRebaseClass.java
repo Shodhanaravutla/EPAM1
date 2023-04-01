73bcdd6ef056a29df0fb2bf4fd308b6d7b019fac
Shodhana <ravutlashodhana@gmail.com>
2023-04-01 13:06:51 +0530

    rebase 1

--- src/MasterRebaseClass.java
+++ src/MasterRebaseClass.java
@@ -1,6 +1,7 @@
 public class MasterRebaseClass {
     public static  void main(String[] args)
     {
-        System.out.println("mater");
+        System.out.println("master");
+        System.out.println("student");
     }
 }
